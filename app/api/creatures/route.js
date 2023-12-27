e1c891163f991432264e31fd15d43327502b5600
William Horn <williamjosephhorn@gmail.com>
2023-12-26 19:52:07 -0500

    api routes

--- app/api/creatures/route.js
+++ app/api/creatures/route.js
@@ -4,25 +4,49 @@ import { NextResponse } from "next/server";
 import Creature from "@/models/creatures";
 
 export async function POST(req) {
-  const body = await req.json();
+  try {
+    await connectMongoDB();
+    const body = await req.json();
 
-  await connectMongoDB();
+    // const newCreature = await Creature.create(body);
+    // await Creature.insertMany(body);
 
-  await Creature.create(body);
-  // await Creature.insertMany(body);
+    return NextResponse.json({
+      message: "Successful Creature Post",
+      // data: newCreature
+    }, { status: 200 });
 
-  return NextResponse.json({
-    status: "Creature data was successfully created"
-  }, {
-    status: 201
-  });
+  } catch (error) {
+
+    return NextResponse.json({
+      message: "Internal Server Error",
+      error
+    }, { status: 500 });
+  }
 }
 
 export async function GET(req) {
-  await connectMongoDB();
-  const creature = await Creature.find();
+  try {
+    await connectMongoDB();
 
-  console.log("creature: ", creature);
+    const params = req.nextUrl.searchParams;
 
-  return NextResponse.json(creature, { status: 201 });
+    const queryParams = {
+      limit: params.get('limit') || 1
+    }
+
+    const creature = await Creature.find({}).limit(queryParams.limit);
+
+    return NextResponse.json(
+      creature, 
+      { status: 200 }
+    );
+
+  } catch(error) {
+
+    return NextResponse.json({ 
+      message: "Internal Server Error", 
+      error 
+    }, { status: 500 });
+  }
 }
